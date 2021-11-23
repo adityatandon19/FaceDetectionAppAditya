458ec68824c7257fe906da2256e345542a8a7e6f
adityatandon19 <68151035+adityatandon19@users.noreply.github.com>
2021-11-23 19:39:43 +0530

    Add files via upload

---
+++ "Procfile"
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2636
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Verdana-Bold;}
+{\colortbl;\red255\green255\blue255;\red26\green26\blue26;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c13333\c13333\c13333;\cssrgb\c100000\c100000\c100000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww16700\viewh7040\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\b\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 web: sh setup.sh && streamlit run FaceDetectionApp.py}
---
+++ setup.sh
@@ -0,0 +1,20 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2636
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Verdana;}
+{\colortbl;\red255\green255\blue255;\red107\green0\blue1;\red255\green255\blue255;\red26\green26\blue26;
+}
+{\*\expandedcolortbl;;\cssrgb\c50196\c0\c0;\cssrgb\c100000\c100000\c100000;\cssrgb\c13333\c13333\c13333;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww24960\viewh18600\viewkind0
+\deftab720
+\pard\pardeftab720\sa520\partightenfactor0
+
+\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+mkdir -p ~/.streamlit/\cf4 \
+\cf2 echo \'93\\\cf4 \
+\cf2 [server]\\n\\\cf4 \
+\cf2 headless = true\\n\\\cf4 \
+\cf2 port = $PORT\\n\\\cf4 \
+\cf2 enableCORS = false\\n\\\cf4 \
+\cf2 \\n\\\cf4 \
+\cf2 \'94 > ~/.streamlit/config.toml\cf4 \
+}
